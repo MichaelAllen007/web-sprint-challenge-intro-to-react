3027d1255f4f4481474dd7577e175cc1c4aab27c
unknown <allenmk20073@yahoo.com>
2021-02-07 12:14:45 -0500

    character cards exported to app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Character from './components/Character'
+import axios from 'axios'
 import './App.css';
 
 const App = () => {
@@ -8,10 +10,23 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [characterInfo, setCharacterInfo] = useState([])
+
+const getCharacters = () => {
+  axios.get('https://swapi.dev/api/people')
+  .then(response => {setCharacterInfo(response.data)})
+}
+
+useEffect(getCharacters, [])
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Star Wars Characters</h1>
+        {
+        characterInfo.map((character, index) => {
+          return <Character character={character} key={index}></Character>
+        })
+      }
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,30 @@
 // Write your Character component here
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledWars = styled.div `
+display: flex;
+text-align: center;
+align-items: center;
+margin-top: 1%;
+flex-wrap: wrap;
+justify-content: space-evenly;
+border: 1px solid black;
+background-color: white;
+`
+
+const Character = (props) => {
+    const {character} = props
+    return (
+        <StyledWars>
+            <h3>Name: {character.name}</h3>
+            <p>DOB: {character.birth_year}</p>
+            <p>Gender: {character.gender}</p>
+            <p>Height: {character.height}</p>
+            <p>Weight: {character.mass}</p>
+            <p>Skin Color: {character.skin_color}</p>
+        </StyledWars>
+    )
+}
+
+export default Character
